2b89ac45d6f62848a28609515dead1b637ce485c
KrithiGanapathy <79409282+KrithiGanapathy@users.noreply.github.com>
2021-10-02 22:13:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var app = fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("It works")
+		return c.SendString("It works 👍")
 	})
 	app.Listen(":8080")
 }
